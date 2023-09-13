3b555ff151ce4b9529378138b432203673af64fd
Sung Paik <sungypaik@gmail.com>
2023-09-13 19:08:25 -0400

    add copy feature of link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,9 @@ const Hero = () => {
         <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
         <button
           type='button'
-          onClick={() => window.open('https://github.com/spaik11')}
+          onClick={() =>
+            window.open('https://github.com/spaik11/ai_summarizer')
+          }
           className='black_btn'>
           GitHub
         </button>
